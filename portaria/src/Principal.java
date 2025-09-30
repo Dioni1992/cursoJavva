f3b54108abc9a022151a40abdce35a132773eb02
DIONI ARLE COELHO DOS SANTOS <arle-dione@hotmail.com>
2025-09-30 11:17:11 -0300

    Inferindo variavel usando var

--- portaria/src/Principal.java
+++ portaria/src/Principal.java
@@ -1,10 +1,10 @@
 public class Principal {
     public static void main(String[] args) {
-        Visitante novoVisitante = new Visitante();
+        var novoVisitante = new Visitante();
         novoVisitante.nome = "Rodnelson";
         novoVisitante.idade = 10;
 
-        CadastroPortaria cadastroPortaria = new CadastroPortaria();
+        var cadastroPortaria = new CadastroPortaria();
         cadastroPortaria.cadastrar(novoVisitante,1);
 
     }
