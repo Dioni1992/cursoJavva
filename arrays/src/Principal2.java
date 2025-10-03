9ef2a43aec3bb66dc63c4d173db32e42ac9b8e40
DIONI ARLE COELHO DOS SANTOS <arle-dione@hotmail.com>
2025-10-03 13:52:24 -0300

    Ordenando numeros

--- arrays/src/Principal2.java
+++ arrays/src/Principal2.java
@@ -1,11 +1,16 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Principal2 {
     public static void main(String[] args) {
-        int[] notas = {8, 5, 4, 9, 10};
+        Integer[] notas = {8, 5, 4, 9, 10};
 
         String notasEmString = Arrays.toString(notas);
-
+        Arrays.sort(notas);
+        Arrays.sort(notas, Comparator.reverseOrder());
         System.out.println(notasEmString);
+
+        System.out.println("Ordenando os numeros crescentes: " +Arrays.toString(notas));
+        System.out.println("Ordenando numeros decrescente: " +Arrays.toString(notas));
     }
 }
