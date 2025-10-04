1643bcf6d7fba2c2a2f83d6a4a113c40bbda141c
DIONI ARLE COELHO DOS SANTOS <arle-dione@hotmail.com>
2025-10-04 15:45:33 -0300

    Arrays

--- arrays/src/Principal3.java
+++ arrays/src/Principal3.java
@@ -13,5 +13,19 @@ public class Principal3 {
         aluno1.nome = "Laura";
         aluno1.idade = 4;
         turmaB.alunos[1] = aluno1;
+
+        turmaB.imprimirListaDeAlunos();
+
+//        for (int i = 0; i < turmaB.alunos.length; i++) {
+//            Aluno aluno = turmaB.alunos[i];
+//            if (aluno != null) {
+//                System.out.printf("%d - %s (%d anos)%n", i, aluno.nome, aluno.idade);
+//
+//            }else {
+//                System.out.printf("%d - vago%n", i);
+//            }
+//
+//        }
+//
     }
 }
